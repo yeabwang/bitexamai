809e86e892bd3d808d6c3ddc04fea98a502f7130
Yeabsira <yeabsiramersha58@yeabfuture.com>
2025-01-10 00:10:48 +0800

    Bit exam ai

--- home/urls.py
+++ home/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from views import home
+from home.views import home
 
 urlpatterns = [
-    path('', home, name='home')
+    path('chat', home, name='home')
 ]
